2b603b32602868060ba26e956eca2f1da276f5e8
nanaho2 <ex1409n@gmail.com>
2023-03-26 14:55:44 +0900

    メモ追加画面のボタンデザインの変更

--- lib/pageSettings.dart
+++ lib/pageSettings.dart
@@ -2,15 +2,21 @@ import 'package:flutter/material.dart';
 
 class Settings extends StatelessWidget {
   const Settings({super.key});
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Icon(Icons.settings_outlined, color: Colors.grey,),
-        title: Text("設定", style: TextStyle(color: Colors.black),),
-        backgroundColor: Colors.grey[200]
-      ),
-    );
+        appBar: AppBar(
+            leading: Icon(
+              Icons.settings_outlined,
+              color: Colors.grey,
+            ),
+            title: Text(
+              "設定",
+              style: TextStyle(color: Colors.black),
+            ),
+            backgroundColor: Colors.grey[200]),
+        body: Text(
+            'JDD | JP + UI Kit Android （© Japan Digital Designクリエイティブ・コモンズ・ライセンス（表示4.0 国際））を改変して作成 https://creativecommons.org/licenses/by/4.0/'));
   }
 }
